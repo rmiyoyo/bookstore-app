a8a1660310e59c0afb70f5ce07c2bbba1071ee60
rmiyoyo <miyoyoraphael@gmail.com>
2023-10-08 00:14:27 +0300

    Fix linter errors

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,31 +5,31 @@ import UserProfile from './user.png';
 import './Navigation.css';
 
 const MenuItem = ({ to, children }) => (
-    <li>
-        <NavLink to={to}>
-            {children}
-        </NavLink>
-    </li>
+  <li>
+    <NavLink to={to}>
+      {children}
+    </NavLink>
+  </li>
 );
 
 MenuItem.propTypes = {
-    to: PropTypes.string.isRequired,
-    children: PropTypes.node.isRequired,
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 const MenuLinks = () => (
-    <header className="nav-container">
-        <h1>Bookstore App</h1>
-        <nav className="menu-links">
-            <ul>
-                <MenuItem to="./">Books</MenuItem>
-                <MenuItem to="/categories">Categories</MenuItem>
-            </ul>
-        </nav>
-        <li className="userProfile">
-            <img src={UserProfile} alt="User Profile" />
-        </li>
-    </header>
+  <header className="nav-container">
+    <h1>Bookstore App</h1>
+    <nav className="menu-links">
+      <ul>
+        <MenuItem to="./">Books</MenuItem>
+        <MenuItem to="/categories">Categories</MenuItem>
+      </ul>
+    </nav>
+    <li className="userProfile">
+      <img src={UserProfile} alt="User Profile" />
+    </li>
+  </header>
 );
 
 export default MenuLinks;
